905809326b3c6cca863dafbad2733019894aed0a
Roman Ahmed <126553951+romanahmed15@users.noreply.github.com>
2023-02-28 14:44:10 +0600

    Add files via upload

---
+++ h3.py
@@ -0,0 +1,9 @@
+# dictionary
+# Exercise 1
+''' d1 = {'roman': 'apple',
+      'rafi': 'doi',
+      'rohan': 'misty',
+      'roni': 'roti',
+      'imran': {'fish', 'meet', 'bif', 'egg'}}
+name = input('enter the name :')
+print(d1[name]) '''
